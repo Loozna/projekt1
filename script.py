8d299549e0b99e5ea79b88168879996bdad241fb
Michał <michal.marciniak90@wp.pl>
2021-04-13 22:47:40 +0200

    Wszystko

--- script.py
+++ script.py
@@ -18,7 +18,22 @@ df.drop(["Serial No."],axis = 1,inplace = True)
 features = df.iloc[:,:-2]
 labels = df.iloc[:,-1]
 labels = labels.to_frame()
-print(features.columns)
-print(labels.columns)
-skalowanie = StandardScaler()
-df_scaled = skalowanie.fit_transform(df)
+
+X_train, X_test, y_train, y_test = train_test_split(
+features, labels, test_size = 0.8)
+
+scaler = StandardScaler()
+features_scaled = scaler.fit_transform(X_train)
+features_test_scaled = scaler.transform(X_test)
+
+model = Sequential()
+model.add(layers.Input(shape=(features_scaled.shape[1])))
+model.add(layers.Dense(32,activation = 'relu'))
+model.add(layers.Dense(32,activation = 'relu'))
+model.add(layers.Dense(1))
+print(model.summary())
+model.compile(loss = 'mse' , optimizer = 'adam', metrics = ['mae'])
+model.fit(features_scaled,y_train,epochs = 20,batch_size = 40)
+model.evaluate(features_test_scaled,y_test,batch_size = 30,
+ verbose = 1
+)
